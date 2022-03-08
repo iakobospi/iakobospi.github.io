71941583a520d82e4fc5313eb0d5df95137eb65a
iakobospi <100960730+iakobospi@users.noreply.github.com>
2022-03-08 23:12:28 +0200

    Add files via upload

---
+++ counter/index.js
@@ -0,0 +1,8 @@
+function increase(){
+    var number = document.getElementById("number");
+    number.innerHTML = parseInt(number.innerHTML) + 1;
+}
+function decrease(){
+    var number = document.getElementById("number");
+    number.innerHTML = number.innerHTML - 1;
+}   
